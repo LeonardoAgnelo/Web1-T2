15808f7226b229e85eb0f4e6abac670f27d1adcc
Samuel Pereira <meninosam197@gmail.com>
2021-10-20 20:00:08 -0300

    first commit

---
+++ src/main/java/br/ufscar/dc/dsw/ExcellentVoyage/ExcellentVoyageApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.ExcellentVoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellentVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcellentVoyageApplication.class, args);
+	}
+
+}
