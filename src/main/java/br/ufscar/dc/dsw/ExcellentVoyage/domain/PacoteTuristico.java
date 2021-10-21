aa90d71925dc8bd3e16dd1ba0b24d2d8dcee4275
Samuel Pereira <meninosam197@gmail.com>
2021-10-20 21:20:52 -0300

    adiciona dominio da compra

--- src/main/java/br/ufscar/dc/dsw/ExcellentVoyage/domain/PacoteTuristico.java
+++ src/main/java/br/ufscar/dc/dsw/ExcellentVoyage/domain/PacoteTuristico.java
@@ -1,11 +1,10 @@
 package br.ufscar.dc.dsw.ExcellentVoyage.domain;
 
 import java.math.BigDecimal;
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
