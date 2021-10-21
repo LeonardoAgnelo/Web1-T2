ef121202098fd402fd2a6d4bfd0df3b4739e0d52
LeonardoAgnelo <leonardoagnelo2001@gmail.com>
2021-10-20 21:16:21 -0300

    adiciona domain Usuario e ValidationMessages

---
+++ src/main/java/br/ufscar/dc/dsw/domain/Usuario.java
@@ -0,0 +1,103 @@
+package br.ufscar.dc.dsw.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "pessoa")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Usuario {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "{NotBlank.user.nome}")
+    @Size(max = 60)
+    @Column(nullable = false, length = 60)
+    private String nome;
+
+    @NotBlank(message = "{NotBlank.user.email}")
+    @Size(max = 60)
+    @Column(nullable = false, length = 60)
+    private String email;
+
+    @NotBlank(message = "{NotBlank.user.senha}")
+    @Size(min = 4, max = 60)
+    @Column(nullable = false, length = 60)
+    private String senha;
+
+    @NotBlank(message = "{NotBlank.user.tipo}")
+    @Size(max = 60)
+    @Column(nullable = false, length = 60)
+    private String tipo;
+
+    public Usuario(Long id){
+        this.setId(id);
+    }
+
+    public Usuario(Long id, String nome, String email, String senha, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public Usuario(String nome, String email, String senha, String tipo) {
+        super();
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
