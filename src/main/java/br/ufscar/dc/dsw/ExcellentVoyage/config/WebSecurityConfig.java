17c09c9ced8451e54ad806e729e2ff767c76d6be
s4mukka <meninosam197@gmail.com>
2021-11-02 15:37:16 -0300

    adiciona dados do usuario na navbar quando logado

--- src/main/java/br/ufscar/dc/dsw/ExcellentVoyage/config/WebSecurityConfig.java
+++ src/main/java/br/ufscar/dc/dsw/ExcellentVoyage/config/WebSecurityConfig.java
@@ -54,6 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.permitAll()
 			.and()
 				.logout()
+				.logoutUrl("/logout")
 				.logoutSuccessUrl("/")
 				.permitAll();
 	}
